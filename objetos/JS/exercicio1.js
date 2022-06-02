cdb0bb291b5c87fe99e796cd12b5312a311c19ee
Lucas da Silva Cruz <102768195+LunicarusIFSP@users.noreply.github.com>
2022-06-02 20:09:52 -0300

    Add files via upload

---
+++ objetos/JS/exercicio1.js
@@ -0,0 +1,8 @@
+function giorjety(){
+    var giorjety = {
+        nome : "Giorgety",
+        idade : 39,
+        cidade : "Boituva"
+    };
+    document.getElementById("nome").innerHTML = giorjety.nome + " tem " +giorjety.idade + " e mora em "+ giorjety.cidade;
+}
