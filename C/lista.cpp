d8b4aa6a12f7177ad6e9e01a6751c17deee2d8b3
Lucas S. Cruz <65511597+lunicarus@users.noreply.github.com>
2022-06-02 18:37:28 -0300

    Update lista.cpp

--- C/lista.cpp
+++ C/lista.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <conio.h>
+#include <math.h>
+	/*Nome dos Integrantes:
+		- Lays Lopes Pereira
+		- Lucas da Silva Cruz
+		- Lucas Moreira Sturchio
+		- Rafael de Souza Lima
+		- Thais de Jesus Silva Rocha */
 
-void Exercicio1() {
+//Exercicios 1,3,5,7
+void Exercicio1() { //lucas
 	//
 	float provas[3], trabalhos[4], totalNotas = 0, totalTrabalhos = 0;
 	printf("digite as notas das provas");
@@ -25,15 +34,61 @@ void Exercicio1() {
 	printf("a media do aluno eh: %.2f", ((totalNotas + totalTrabalhos) / 7));
 }
 
-void Exercicio3() {
+void Exercicio3() { //lucas
 	float F, C;
 	printf("informe a temperatura em Celcius");
 	scanf_s("%f", &C);
 	F = (C * 1.8F) + 32;
 	printf("temperatura convertida em Fahenheit: %.2f\F",F);
 }
-
-void Exercicio7() {
+void Exercicio5() { //Thais
+    int mes;
+    
+    printf("digite um numero de 1 a 12:");
+    scanf("%d",&mes);
+    
+    if(mes==1)
+        printf("janeiro\n");
+    else
+    if(mes==2)
+        printf("fevereiro\n");
+    else
+    if(mes==3)
+        printf("março\n");
+    else    
+    if (mes==4)
+        printf("abril\n");
+    else    
+    if (mes==5) 
+        printf("maio\n");
+    else
+    if (mes==6)
+        printf("junho\n");
+    else
+    if (mes==7)
+        printf("julho\n");
+    else    
+    if (mes==8)
+        printf("agosto\n");
+    else
+    if (mes==9)
+        printf("setembro\n");
+    else
+    if (mes==10)
+        printf("outubro\n");
+    else    
+    if (mes==11)
+        printf("novembro\n");
+    else    
+    if (mes==12)
+        printf("dezembro\n");
+    else
+        printf("mes invalido");
+        
+    getch();
+    system("pause");
+}
+void Exercicio7() { //lucas
 		int a = 0, b = 1, total = 0;
 		printf("1 ");
 		for (int i = 0; i < 29; i++) {
@@ -44,37 +99,7 @@ void Exercicio7() {
 		}
 }
 
-#include <iostream>
-
-
-void Desafio1() {
-	float matriz[4][5];
-	float maior = 0;
-	float menor;
-	float aux;
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			printf("informe o valor da posicao matriz[%d][%d]:", i, j);
-			scanf_s("%f", &matriz[i][j]);
-			if (matriz[i][j] > maior) {
-				maior = matriz[i][j];
-
-				if (matriz[i][j] < menor) {
-					menor = matriz[i][j];
-				}
-
-			}
-			float menor = matriz[0][0];
-		}
-		printf("Maior: %f\nMenor: %f", maior, menor);
-	}
-}
 int main() {
-	Desafio1();
-	}
-int main() {
-	Desafio1();
+
 	return 0;
 }
